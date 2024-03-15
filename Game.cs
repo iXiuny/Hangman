ada36d13d20bc3010c2dc18ac5a44c62e2e452c9
iXiuny <83838372+iXiuny@users.noreply.github.com>
2024-03-15 21:11:09 +0100

    Source code & Images
    Zip file coming soon

---
+++ Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Hangman
+{
+    internal class Game
+    {
+        static int lives = 10;
+        static int livesKeeper = 10;
+        private string _extracted;
+        private string _extractedKeeper;
+        string HiddenExtract;
+        char[] ExtractedCharAr;
+        char[] HiddenExtractedCharAr;
+        char PlayerChoicee;
+        public int showlives { get { return lives; } }
+        public char PlayerChoice { get; set; }
+        public string HiddenExtraction { get  { return HiddenExtract; } }
+        public string Extracted { get { return _extracted; } }
+        public string ExtractionKeeper { get { return _extractedKeeper; } }
+        public Game(int txtcount) 
+        {
+            Random rnd = new Random();
+            int Extract = rnd.Next(1, txtcount);
+            using (StreamReader TxtRead = new StreamReader("parole.txt")) {
+                for (int i = 0; i < Extract; i++) {
+                    TxtRead.ReadLine();
+                }
+                _extracted = TxtRead.ReadLine();
+                HiddenExtract = _extracted;
+                _extractedKeeper = _extracted;
+                HiddenExtractedCharAr = HiddenExtract.ToCharArray();
+
+                for (int i = 0; i< HiddenExtract.Length; i++)
+                {
+                    if (HiddenExtractedCharAr[i] == 'a' || HiddenExtractedCharAr[i] == 'e' || HiddenExtractedCharAr[i] == 'i' || HiddenExtractedCharAr[i] == 'o' || HiddenExtractedCharAr[i] == 'u')
+                    {
+                        HiddenExtractedCharAr[i] = '+';
+                    }
+                    else
+                    {
+                        HiddenExtractedCharAr[i] = '-';
+                    }
+                    HiddenExtractedCharAr[0] = HiddenExtract[0];
+                    HiddenExtractedCharAr[HiddenExtractedCharAr.Length - 1] = HiddenExtract[HiddenExtractedCharAr.Length-1];
+                }
+                HiddenExtract = new string(HiddenExtractedCharAr);
+                ExtractedCharAr = _extracted.ToCharArray();
+            }
+        }
+        public void ExtractionGuess()
+        {
+            if (Extracted.Contains(PlayerChoice))
+            {
+                for (int i = 0; i < ExtractedCharAr.Length; i++)
+                {
+                    if (ExtractedCharAr[i] == PlayerChoice)
+                    {
+                        HiddenExtractedCharAr[i] = PlayerChoice;
+                        HiddenExtract = new string(HiddenExtractedCharAr);
+                    }
+                }
+            }
+            else
+            {
+                lives--;
+            }
+        }
+        public void RestartGame()
+        {
+            lives = livesKeeper;
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _game = new Game(ReadTxtFile());
+            lbl_hiddenword.Content = string.Empty; //Da portare HiddenWord da Public game
+            txtb_choice.Visibility = Visibility.Collapsed;
+            lbl_lives.Visibility= Visibility.Collapsed;
+            lbl_lives.Content = _game.showlives;
+            btn_playagain.Visibility = Visibility.Collapsed;
+        }
+
+        public int ReadTxtFile()
+        {
+            int txtcount=0;
+            using (StreamReader TxtRead = new StreamReader("parole.txt"))
+            {
+                string txtonread;
+                while ((txtonread = TxtRead.ReadLine()) != null)
+                {
+                    txtcount++;
+                }
+                //lblTestOutput.Content = txtcount.ToString();
+            }
+            return txtcount;
+        }
+
+        private void btn_play_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_hiddenword.Content = _game.HiddenExtraction;
+            txtb_choice.Visibility=Visibility.Visible;
+            lbl_lives.Visibility = Visibility.Visible;
+            btn_play.Visibility = Visibility.Collapsed;   
+        }
+        private void btn_playAgain_Click(object sender, RoutedEventArgs e)
+        {
+            _game = new Game(ReadTxtFile());
+            _game.RestartGame();
+            lbl_lives.Content = _game.showlives;
+            lbl_hiddenword.Content= _game.HiddenExtraction;
+            txtb_choice.Visibility = Visibility.Visible;
+            btn_playagain.Visibility= Visibility.Collapsed;
+            background.Source = new BitmapImage(new Uri("Images/Hangman" + _game.showlives + "livesleft.png", UriKind.Relative));
+
+        }
+
+
+        private void txtb_choice_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _game.ExtractionGuess();
+                lbl_lives.Content = _game.showlives;
+                lbl_hiddenword.Content = _game.HiddenExtraction;
+                if (_game.HiddenExtraction == _game.ExtractionKeeper)
+                {
+                    lbl_hiddenword.Content = "You Won!";
+                    txtb_choice.Visibility = Visibility.Collapsed;
+                    btn_playagain.Visibility = Visibility.Visible;
+                    btn_playagain.Background = Brushes.Green;
+                    btn_play.Content = "Play Again";
+                }
+                else if (_game.showlives == 0)
+                {
+                    lbl_hiddenword.Content = "You Lost!";
+                    txtb_choice.Visibility = Visibility.Collapsed;
+                    background.Source = new BitmapImage(new Uri("Images/Hangman0livesleft.png", UriKind.Relative));
+                    btn_playagain.Background = Brushes.Red;
+                    btn_playagain.Visibility= Visibility.Visible;
+                    btn_play.Content = "Retry";
+                }
+                else
+                {
+                    background.Source = new BitmapImage(new Uri("Images/Hangman" + _game.showlives + "livesleft.png", UriKind.Relative)) ;
+                }
+            }
+        }
+        private void txtb_choice_TextChanged(object sender, TextChangedEventArgs e) {
+            lbl_Error.Visibility = Visibility.Collapsed;
+            try
+            {
+                _game.PlayerChoice = Convert.ToChar(txtb_choice.Text);
+            }
+            catch
+            {
+                txtb_choice.Text = "";
+                lbl_Error.Visibility = Visibility.Visible;
+                lbl_Error.Content = "A character is required in order to play";
+            }
+
+        }
+    }
+}
